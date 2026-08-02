62078a5ce42861da5abd2077047747fac2c41340
Varun S Mogaveera <varunmogaveera2005@gmail.com>
2026-08-02 12:45:51 +0530

    fix: register Chart.js ArcElement and plugins for donut chart

--- frontend/components/LanguageBreakdown.tsx
+++ frontend/components/LanguageBreakdown.tsx
@@ -2,6 +2,9 @@
 
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 type Item = { language?: string; file_count?: number };
 
